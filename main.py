d19182aee7eb9c0b4443a48e11c28dc4e4691c16
Caesario Dito <caesariodito@gmail.com>
2024-03-13 18:22:03 +0700

    feat: created the streamlit website
    fully working on local

--- main.py
+++ main.py
@@ -1,219 +1,63 @@
 import streamlit as st
 import pandas as pd
+import functions as f
+from PIL import Image
 
-st.set_page_config(layout="wide")
+thumbnail = Image.open("./docs/thumbnail.jpg")
+logo = Image.open("./docs/logo.png")
+
+# PAGE CONFIG
+st.set_page_config(layout="centered", page_icon=logo, page_title="OCR System")
+
+# SESSION CONFIG
+st.session_state.disabled = True
+st.session_state.clicked = False
 
 # Thumbnail for PLN
+st.image(thumbnail, use_column_width=True)
 st.title("OCR System")
 
-col1, col2 = st.columns(2)
-col1.header("Labels here")
-labels = col1.text_area("input your labels here", key="labels")
+# col1, col2 = st.columns(2)
+# col1.header("Labels here")
+# labels = col1.text_area("input your labels here", key="labels")
 
-col2.header("Image links here")
-image_links = col2.text_area("input your links of images here", key="image_links")
+# col2.header("Image links here")
+# image_links = col2.text_area("input your links of images here", key="image_links")
 
-st.button("Submit")
+placeholder_image_link = """https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322722109343&nomor_meter=null&blth=202403
+https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700119528&nomor_meter=null&blth=202403
+https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700134188&nomor_meter=null&blth=202403
+"""
+st.header("Image links here")
+image_links = st.text_area(
+    "input your links of images here",
+    key="image_links",
+    placeholder=placeholder_image_link,
+)
+
+if image_links.replace(" ", "") != "":
+    st.session_state.disabled = False
+
+st.write("Ctrl + Enter to read the link of images")
 st.divider()
 
+st.session_state.clicked = not st.session_state.get("disabled")
+if st.session_state.get("clicked"):
+    predictions = f.perform_ocr(image_links)
 
-data = [
-    [
-        "Cat",
-        "Dog",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Dog",
-        "Dog",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Apple",
-        "Banana",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Car",
-        "Truck",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Person",
-        "Bicycle",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "House",
-        "Building",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Tree",
-        "Plant",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Flower",
-        "Rose",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Shirt",
-        "Dress",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Book",
-        "Pen",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Cloud",
-        "Sky",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Sun",
-        "Moon",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Star",
-        "Planet",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Bird",
-        "Animal",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Fish",
-        "Sea",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Bicycle",
-        "Vehicle",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Car",
-        "Truck",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Motorcycle",
-        "Scooter",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Train",
-        "Bus",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Airplane",
-        "Helicopter",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Table",
-        "Chair",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Cup",
-        "Mug",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Plate",
-        "Bowl",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Fork",
-        "Spoon",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Knife",
-        "Scissors",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Pen",
-        "Pencil",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Paper",
-        "Notebook",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Computer",
-        "Laptop",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Phone",
-        "Tablet",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-    [
-        "Watch",
-        "Clock",
-        False,
-        "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-    ],
-]
-
-# Create a DataFrame
-df = pd.DataFrame(data, columns=["Label", "Predictions", "Correct", "ImageLink"])
-
-# only unlock Label columns
-st.data_editor(
-    df,
-    num_rows="dynamic",
-    use_container_width=True,
-    column_config={
-        "Correct": st.column_config.CheckboxColumn(
-            "Correct", help="Check if the prediction is correct"
-        ),
-        "ImageLink": st.column_config.ImageColumn(
-            "Preview Image", help="Image data preview"
-        ),
-    },
-)
+    df = f.generate_dataframe(image_links, predictions)
+    st.data_editor(
+        df,
+        num_rows="dynamic",
+        use_container_width=True,
+        column_config={
+            "Status": st.column_config.CheckboxColumn(
+                "Status", help="Check if the prediction is correct"
+            ),
+            "ImageURL": st.column_config.ImageColumn(
+                "ImageURL", help="Image data preview"
+            ),
+        },
+    )
+else:
+    st.write("Please input the link of images and the table will be shown here.")
