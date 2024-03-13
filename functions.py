5300ebdb3b846bf30032886ac893d8e47eb43f7a
Caesario Dito <caesariodito@gmail.com>
2024-03-13 20:29:33 +0700

    feat: new features for perfomance metrics

--- functions.py
+++ functions.py
@@ -1,18 +1,59 @@
 import pandas as pd
 import requests
+from datetime import datetime
+import typing
 from PIL import Image
 from ultralytics import YOLO
+import os
 
 import streamlit as st
 
 
-COLUMNS = ["ImageURL", "Status", "Hasil OCR", "Blth", "IDPEL"]
-model_path = "./model/ocr_best.pt"
-model = YOLO(model_path, task="detect")
+@st.cache_resource
+def init_model(model_path):
+    model = YOLO(model_path, task="detect")
+    new_path = "./model/ocr_best.torchscript"
+    if not os.path.exists(new_path):
+        model.export(
+            optimize=True,
+            half=True,
+            int8=True,
+            dynamic=True,
+            simplify=True,
+        )
+
+    model = YOLO(new_path, task="detect")
+    return model
+
+
+def timer(start_time: datetime = None) -> "typing.Union[datetime.datetime, str]":
+    """
+    Measures the time elapsed from a given start time.
+
+    If no start time is provided, returns the current time. If a start time is provided, returns a formatted string
+    representing the time elapsed from the start time to the current time.
+
+    Args:
+        start_time (datetime.datetime, optional): The start time to measure elapsed time from, or None to get the current time. Defaults to None.
+
+    Returns:
+        Union[datetime.datetime, str]: The current time if no start time is provided, or a formatted string representing the elapsed time.
+    """
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+        tmin, tsec = divmod(temp_sec, 60)
+        return "%i hours %i minutes and %s seconds." % (
+            thour,
+            tmin,
+            round(tsec, 2),
+        )
 
 
 @st.cache_data(show_spinner=False)
-def generate_dataframe(links: str, predictions: list) -> pd.DataFrame:
+def generate_dataframe(links: str, predictions: list, columns: list) -> pd.DataFrame:
     total_row = len(links.split("\n"))
 
     statuses = [False] * total_row
@@ -26,7 +67,7 @@ def generate_dataframe(links: str, predictions: list) -> pd.DataFrame:
             "Blth": blth,
             "IDPEL": idpel,
         },
-        columns=COLUMNS,
+        columns=columns,
     )
 
     return df
@@ -34,10 +75,13 @@ def generate_dataframe(links: str, predictions: list) -> pd.DataFrame:
 
 @st.cache_data(show_spinner=False)
 def load_image_from_url(url):
-    return Image.open(requests.get(url, stream=True).raw)
+    result = Image.open(requests.get(url, stream=True).raw)
+    return result
 
 
-def perform_ocr(links: str) -> list:
+def perform_ocr(links: str, model, names) -> list:
+    ocr_timer = timer(None)
+
     labels = []
     urls = links.split("\n")
     my_bar = st.progress(0, text="Performing OCR...")
@@ -45,31 +89,39 @@ def perform_ocr(links: str) -> list:
     for i, url in enumerate(urls):
         my_bar.progress(int(i * 100 / len(urls)), text="Performing OCR...")
 
+        load_timer = timer(None)
         im = load_image_from_url(url)
+        st.write(f"Time to load image: {timer(load_timer)}")
 
-        results = predict(im)
+        predict_timer = timer(None)
+        results = predict(im, model)
+        st.write(f"Time to predict image: {timer(predict_timer)}")
+
         for result in results:
-            label = get_text(result)
+            label = get_text(result, names)
             labels.append(label)
 
     my_bar.empty()
 
+    st.write(f"Time to perform OCR: {timer(ocr_timer)}")
+
     return labels
 
 
-def predict(_image):
-    return model(source=_image, stream=True)
+def predict(_image, model):
+    result = model.predict(source=_image, stream=False, verbose=False)
+    return result
 
 
-def get_text(result):
+def get_text(result, names):
     boxes = result.boxes.xyxy
-    cls = result.boxes.cls
+    classes = result.boxes.cls
 
     sorted_indices = boxes[:, 0].argsort()
     # sorted_boxes = boxes[sorted_indices]
-    sorted_cls = cls[sorted_indices]
+    sorted_cls = classes[sorted_indices]
 
-    file_labels = [model.names[int(c)] for c in sorted_cls]
+    file_labels = [names[int(c)] for c in sorted_cls]
     label = "".join(file_labels)
 
     return label
--- main.py
+++ main.py
@@ -2,61 +2,65 @@ import streamlit as st
 import functions as f
 from PIL import Image
 
-thumbnail = Image.open("./docs/thumbnail.jpg")
-logo = Image.open("./docs/logo.png")
 
-# PAGE CONFIG
-st.set_page_config(layout="centered", page_icon=logo, page_title="OCR System")
+def main():
+    # PAGE CONFIG
+    thumbnail = Image.open("./docs/thumbnail.jpg")
+    logo = Image.open("./docs/logo.png")
+    st.set_page_config(layout="centered", page_icon=logo, page_title="OCR System")
 
-# SESSION CONFIG
-st.session_state.disabled = True
-st.session_state.clicked = False
+    # INITS
+    COLUMNS = ["ImageURL", "Status", "Hasil OCR", "Blth", "IDPEL"]
+    NAMES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    model_path = "./model/ocr_best.pt"
+    model = f.init_model(model_path)
 
-# Thumbnail for PLN
-st.image(thumbnail, use_column_width=True)
-st.title("OCR System")
+    # SESSION CONFIG
+    st.session_state.disabled = True
+    st.session_state.clicked = False
 
-# col1, col2 = st.columns(2)
-# col1.header("Labels here")
-# labels = col1.text_area("input your labels here", key="labels")
+    # Thumbnail for PLN
+    st.image(thumbnail, use_column_width=True)
+    st.title("OCR System")
 
-# col2.header("Image links here")
-# image_links = col2.text_area("input your links of images here", key="image_links")
-
-placeholder_image_link = """https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322722109343&nomor_meter=null&blth=202403
-https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700119528&nomor_meter=null&blth=202403
-https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700134188&nomor_meter=null&blth=202403
-"""
-st.header("Image links here")
-image_links = st.text_area(
-    "input your links of images here",
-    key="image_links",
-    placeholder=placeholder_image_link,
-)
-
-if image_links.replace(" ", "") != "":
-    st.session_state.disabled = False
-
-st.write("Ctrl + Enter to read the link of images")
-st.divider()
-
-st.session_state.clicked = not st.session_state.get("disabled")
-if st.session_state.get("clicked"):
-    predictions = f.perform_ocr(image_links)
-
-    df = f.generate_dataframe(image_links, predictions)
-    st.data_editor(
-        df,
-        num_rows="dynamic",
-        use_container_width=True,
-        column_config={
-            "Status": st.column_config.CheckboxColumn(
-                "Status", help="Check if the prediction is correct"
-            ),
-            "ImageURL": st.column_config.ImageColumn(
-                "ImageURL", help="Image data preview"
-            ),
-        },
+    placeholder_image_link = """https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322722109343&nomor_meter=null&blth=202403
+    https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700119528&nomor_meter=null&blth=202403
+    https://portalapp.iconpln.co.id/acmt/DisplayBlobServlet1?idpel=322700134188&nomor_meter=null&blth=202403
+    """
+    st.header("Image links here")
+    image_links = st.text_area(
+        "input your links of images here",
+        key="image_links",
+        placeholder=placeholder_image_link,
     )
-else:
-    st.write("Please input the link of images and the table will be shown here.")
+
+    if image_links.replace(" ", "") != "":
+        st.session_state.disabled = False
+
+    st.write("Ctrl + Enter to read the link of images")
+    st.divider()
+
+    st.session_state.clicked = not st.session_state.get("disabled")
+    if st.session_state.get("clicked"):
+        predictions = f.perform_ocr(image_links, model, NAMES)
+
+        df = f.generate_dataframe(image_links, predictions, COLUMNS)
+        st.data_editor(
+            df,
+            num_rows="dynamic",
+            use_container_width=True,
+            column_config={
+                "Status": st.column_config.CheckboxColumn(
+                    "Status", help="Check if the prediction is correct"
+                ),
+                "ImageURL": st.column_config.ImageColumn(
+                    "ImageURL", help="Image data preview"
+                ),
+            },
+        )
+    else:
+        st.write("Please input the link of images and the table will be shown here.")
+
+
+if __name__ == "__main__":
+    main()
